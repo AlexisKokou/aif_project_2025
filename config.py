bee8d7b6aef56ffd28ae6818ba2d6bbd38255c2b
abou0117 <sidikibah100@gmail.com>
2026-02-03 14:56:52 +0100

    merging in progress

---
+++ config.py
@@ -0,0 +1,4 @@
+GENRES_MAPPING = {'action': 0, 'animation': 1, 'comedy': 2, 'documentary': 3, 'drama': 4, 'fantasy': 5, 'horror': 6, 'romance': 7, 'science Fiction': 8, 'thriller': 9}
+GENRES_MAPPING_INV = {0: 'action', 1: 'animation', 2: 'comedy', 3: 'documentary', 4: 'drama', 5: 'fantasy', 6: 'horror', 7: 'romance', 8: 'science Fiction', 9: 'thriller'}
+NUM_GENRES = 10
+EMBEDDING_DIM = 768
